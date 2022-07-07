bef9db3d06f33b85dd3d4d1b772e8d9813a5a1e1
MrRandom <shayan78simaei@gmail.com>
2022-07-08 02:42:43 +0430

    build fix

---
+++ app/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const ipc = require('electron').ipcRenderer
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 300,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        autoHideMenuBar: true,
+
+
+    });
+
+    win.resizable = false;
+    win.on('minimize', function(event) {
+        event.preventDefault();
+        win.hide();
+    });
+
+    win.on('close', function(event) {
+        if (!win.isQuiting) {
+            event.preventDefault();
+            win.hide();
+        }
+
+        return false;
+    });
+    win.loadFile(__dirname + '/index.html');
+    win.webContents.on('did-finish-load', () => {
+
+    });
+};
+
+app.whenReady().then(async() => {
+    await createWindow();
+
+})
